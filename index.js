d657bf4be07949a6c178a8b0607b1213fb1e2e04
Dilly212 <Dwaltersaztec@gmail.com>
2026-01-08 10:43:44 -0700

    .

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
-/**
- * Validate.js
- * Runs a series of tests, to display status of the configured package.
- * - Get OAuth Access Token
- * - Test BC v2 API
- * - Test BC Web Services API
- */
-
 const Connection = require("./src/scripts/validate");
+const { getAccessToken } = require("./src/config/OAuth");
 const BC = require("./src/modules/BC.v1.module");
 const BCv2 = require("./src/modules/BC.v2.module");
-const { getAccessToken } = require("./src/config/OAuth");
+const Api = require("./src/api/BC.api");
 
 
-module.exports = { getAccessToken, Connection, BCv2, BC };
+module.exports = { getAccessToken, Connection, BCv2, BC, Api };
